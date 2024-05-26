de52d70a799399c8a42565fe6e4088935113838e
cxl258 <3288270459@qq.com>
2024-05-26 19:05:48 +0800

    修改

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,25 @@
-import request from '../utils/request'
+import request from "../utils/request";
 
-export const login = (data:any) => {
-    return request.request({
-        url:'/api/login',
-        method:"post",
-        data
-    })
-}
-
-export const yiyan = ()=>{
-    return request.request({
-        url:'/api/yiiyan',
-        method:"get"
-    })
-}
+// 注册功能实现
+export const register = (data: any) => {
+  return request.request({
+    url: "/api/reguser",
+    method: "post",
+    data,
+  });
+};
+// 登录功能实现
+export const login = (data: any) => {
+  return request.request({
+    url: "/api/login",
+    method: "post",
+    data,
+  });
+};
+// 获取随机一句话
+export const yiyan = () => {
+  return request.request({
+    url: "/api/yiiyan",
+    method: "get",
+  });
+};
